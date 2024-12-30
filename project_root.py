59e3275f60eb4afc6092387afb2bb8ec8f77d96a
Skaiste Motiejunaite <s.motiejunaite1@universityofgalway.ie>
2024-12-30 11:43:59 +0000

    project root file that degines most commonly used paths

---
+++ project_root.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent / "DevCode"
+
+#commonly used paths
+ALGORITHMS_DIR = PROJECT_ROOT / "Algorithms"
+DATASETS_DIR = PROJECT_ROOT / "Datasets"
+APP_DIR = PROJECT_ROOT / "App"
